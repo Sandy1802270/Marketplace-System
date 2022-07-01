f8323151182e643cbd8ab8d4ff80db3cf74d8fe1
YoussefHekal <85392431+YoussefHekal@users.noreply.github.com>
2022-07-01 23:34:01 +0200

    Add files via upload

---
+++ Client/User.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.market;
+
+
+
+public class User {
+     static String username,pass;
+
+    public User(String username, String pass) {
+        this.username = username;
+        this.pass = pass;
+    }
+
+    static public String getUsername() {
+        return username;
+    }
+
+    static public String getPass() {
+        return pass;
+    }
+
+    static public void setUsername(String username) {
+        User.username = username;
+    }
+
+    static public void setPass(String pass) {
+        User.pass = pass;
+    }
+    
+    
+}
