1109fe2f73b17e6e85487ef7859688097776f0e3
Youssif Khaled Shawky <85262382+youssif1807198@users.noreply.github.com>
2022-07-02 10:15:54 +0200

    Add files via upload

--- Server/Server.java
+++ Server/Server.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
 /**
- *
  * @author ofaof
  */
 /*
@@ -46,7 +46,7 @@ public class Server {
                 // to server
                 System.out.println("New client connected"
                         + client.getInetAddress()
-                                .getHostAddress());
+                        .getHostAddress());
 
                 // create a new thread object
                 ClientHandler clientSock
@@ -102,6 +102,79 @@ public class Server {
                     }
 
                     switch (Integer.parseInt(line)) {
+                        case(11):{ //food
+
+//                            a.add(pic);
+//                            a.add(iname);
+//                            a.add(qty);
+//                            a.add(price);
+                            Functions f=new Functions();
+                            ArrayList<String> a = new ArrayList<>();
+
+                            f.caseFood(a);
+                            out.println(String.valueOf(a.size()));
+
+                            for (int i = 0; i < a.size(); i++) {
+                                out.println(a.get(i));
+                            }
+                        }
+
+                        case(12):{ //food
+
+//                            a.add(pic);
+//                            a.add(iname);
+//                            a.add(qty);
+//                            a.add(price);
+                            Functions f=new Functions();
+                            ArrayList<String> a = new ArrayList<>();
+
+                            f.caseCloth(a);
+                            out.println(String.valueOf(a.size()));
+
+                            for (int i = 0; i < a.size(); i++) {
+                                out.println(a.get(i));
+                            }
+                        }
+                        case(13):{ //food
+
+//                            a.add(pic);
+//                            a.add(iname);
+//                            a.add(qty);
+//                            a.add(price);
+                            Functions f=new Functions();
+                            ArrayList<String> a = new ArrayList<>();
+
+                            f.caseElec(a);
+                            out.println(String.valueOf(a.size()));
+
+                            for (int i = 0; i < a.size(); i++) {
+                                out.println(a.get(i));
+                            }
+                        }
+                        case(14):{ //food
+
+//                            a.add(pic);
+//                            a.add(iname);
+//                            a.add(qty);
+//                            a.add(price);
+                            Functions f=new Functions();
+                            ArrayList<String> a = new ArrayList<>();
+
+                            f.caseHA(a);
+                            out.println(String.valueOf(a.size()));
+
+                            for (int i = 0; i < a.size(); i++) {
+                                out.println(a.get(i));
+                            }
+                        }
+                        case(20):{
+                            Functions f=new Functions();
+                            int sum=0;
+                            out.println(f.case20(sum));
+                            sum=0;
+                            out.println(f.case21(sum));
+                        }
+
 
                         case (0)://login 
                         {
@@ -121,57 +194,55 @@ public class Server {
                             String s = f.case1(USER_ID, password, fname, lname);
                             out.println(s);
                         }
-//                        case (1): //create new account
-//                        {
-//                          String USER_ID = in.readLine();
-//                          String password = in.readLine();
-//                          
-//                          /*ADD USER ID AND PASSWORD TO THE DATABASE*/
 
-//                        case (2): {
-//                            String item_ID = in.readLine();
-//                            String quantity = in.readLine();
-//                            /*check at DATABASE*/
-//                            if ("123".equals(item_ID) && "4".equals(quantity)) {
-//                                out.println("Added");
-//                            } else {
-//                                out.println("Not added");
-//
-//                            }
-//                        }
-//                        case (3): {
-//                            String item_ID = in.readLine();
-//                            String quantity = in.readLine();
-//                            /*check at DATABASE*/
-//                            if ("123".equals(item_ID) && "4".equals(quantity)) {
-//                                out.println("Edited");
-//                            } else {
-//                                out.println("Not edited");
-//
-//                            }
-//                        }
-//                        case (4): {
-//                            String item_ID = in.readLine();
-//                            String quantity = in.readLine();
-//                            /*check at DATABASE*/
-//                            if ("123".equals(item_ID) && "4".equals(quantity)) {
-//                                out.println("Removed");
-//                            } else {
-//                                out.println("Not removed");
-//                            }
-//                        }
                         case (5): { //view balance
                             String id = in.readLine();
                             Functions f = new Functions();
                             out.println(f.case5(id));
-                            /*check at DATABASE for the password and display info*/
                         }
-                        case (6): {
+                        case (52): { //view balance
+                            String id = in.readLine();
+                            Functions f = new Functions();
+                            out.println(f.case5(id));
 
+                            ArrayList<String> oid = new ArrayList<>();
+                            ArrayList<String> tot = new ArrayList<>();
+                            f.case52(id, oid, tot);
+                            out.println(String.valueOf(oid.size()));
+                            for (int i = 0; i < oid.size(); i++) {
+                                out.println(oid.get(i));
+                                out.println(tot.get(i));
+                            }
+                        }
+                        case (7): {//search
+                            String iname = in.readLine();
+                            /*check the DATABASE for the item name*/
+                            Functions f = new Functions();
+                            out.println(f.case7(iname));
+                        }
+                        case (8): {//deposit
+                            String id = in.readLine();
+                            String amount = in.readLine();
+                            Functions f = new Functions();
+                            out.println(f.case8(id, amount));
+                        }
+                        case (9): {
+                            String iname = in.readLine();
+                            //database check
+                            Functions f = new Functions();
+                            ArrayList<String> IP = new ArrayList<>();
+                            f.case9(iname, IP);
+                            out.println(IP.get(0));//id
+                            out.println(IP.get(1));//price
+//                            out.println();
+                        }
+                        case (10): {
+                            System.out.println("in case 6 now");
                             String USER_ID = in.readLine();
-                            int USER_balance = 4000;
-                            int totalprice = 0;
-
+                            Functions f = new Functions();
+                            int USER_balance = Integer.parseInt(f.case61(USER_ID));
+                            float totalprice = 0;
+                            System.out.println("f.case61 now");
                             int array_size = Integer.parseInt(in.readLine());
                             ArrayList<String> items_ID = new ArrayList<>();
                             ArrayList<String> quantity = new ArrayList<>();
@@ -181,67 +252,39 @@ public class Server {
 
                                 items_ID.add(in.readLine());
                                 quantity.add(in.readLine());
+                                price.add(in.readLine());
 
                             }
 
-                            /*database will fill the array of prices*/
-//                            price.add("20");
-//                            price.add("30");
-//                            price.add("40");
-//                            
+                            System.out.println("after for loop now");
                             for (int i = 0; i < array_size; i++) {
-
-                                totalprice += Integer.parseInt(price.get(i)) * Integer.parseInt(quantity.get(i));
+                                price.set(i, price.get(i).substring(1, price.get(i).length()));
 
                             }
 
+                            for (int i = 0; i < array_size; i++) {
+                                System.out.println(price.get(i));
+
+                            }
+                            for (int i = 0; i < array_size; i++) {
+                                totalprice += Float.parseFloat(price.get(i)) * Float.parseFloat(quantity.get(i));
+                            }
                             /*check the DATABASE for the available cash*/
-                            if (USER_balance >= totalprice) {
-
+                            if ((float) USER_balance >= totalprice) {
+                                /*decrease qty of items from database*/
+                                f.case62(USER_ID, items_ID, quantity, (int) totalprice, USER_ID);
                                 out.println("okay");
-
-                                /*remove this items from database*/
+                                System.out.println("b>t now");
                                 //decrease balance in database
+                                //insert orders
                             } else {
                                 out.println("not okay");
-
+                                System.out.println("b<t");
                             }
-
                         }
-                        case (7): {//search
-                            String iname = in.readLine();
-                            /*check the DATABASE for the item name*/
-                            Functions f = new Functions();
-                            out.println(f.case7(iname));    
-                        }
-                        case (8): {//deposit
-                            String id = in.readLine();
-                            String amount = in.readLine();
-                            Functions f = new Functions();
-                            out.println(f.case8(id,amount)); 
-                        }
-                        case(9):{
-                            String iname=in.readLine();
-                            //database check
-                            Functions f = new Functions();
-                            ArrayList<String> IP = new ArrayList<>();
-                            f.case9(iname,IP);
-                            out.println(IP.get(0));//id
-                            out.println(IP.get(1));//price
-
-                            
-                            
-
-//                            out.println();
-                        
-                        }
-                            
-
                         default: {
                         }
-
                     }
-
                 }
             } catch (IOException e) {
                 e.printStackTrace();
